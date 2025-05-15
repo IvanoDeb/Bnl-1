8dfa26c72423fc8c7cf936f8a5d0ffd655de971d
IvanoDeb <ivano.debeljak@gmail.com>
2025-05-16 00:36:51 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import os
+import discord
+from discord.ext import commands
+from aiohttp import web
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot je online kao {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+async def handle(request):
+    return web.Response(text="Bot radi.")
+
+app = web.Application()
+app.add_routes([web.get('/', handle)])
+
+async def start_web():
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', int(os.environ.get("PORT", 8080)))
+    await site.start()
+
+asyncio.get_event_loop().create_task(start_web())
+
+bot.run(os.getenv("DISCORD_TOKEN"))
